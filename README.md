b65f61ce92f7172b01430aa62ab765badf35265d
AntonLapshin <antvallap@gmail.com>
2022-11-24 19:58:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-mfe1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-w6ahnt)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className="App">MFE1</div>;
+}
+
+export default App;
